db42404e167b2d1cad4f836ed8bc7b5f6bb5493c
eupatheia <75283980+eupatheia@users.noreply.github.com>
2022-01-21 15:04:10 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,14 @@
+/* ***
+ * fortune.c
+ * Jasmine Lei
+ * 01.21.22
+ * Prints a fortune
+ * ***
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Something may or may not happen sometime in the future.\n");
+  return 0;
+}
