d6c16a122386b59dee3ff8a778e9294db93688bd
eupatheia <75283980+eupatheia@users.noreply.github.com>
2022-02-11 23:57:34 -0500

    finished magic_square, working on sorted_snackbar

--- A03/magic_square.c
+++ A03/magic_square.c
@@ -1,12 +1,113 @@
 //---------------------------------------------------------------------
-// magic_square.c 
+// magic_square.c
 // CS223 - Spring 2022
 // Identify whether a matrix is a magic square
-// Name:
+// Name: Jasmine Lei
+// Date: 11 February 2022
 //
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-}
+  int rows, cols;
+  int magic_const = 0;
+  int* matrix = NULL;
 
+  scanf(" %d", &rows);
+  scanf(" %d", &cols);
+
+  // must be an n x n matrix
+  if (rows != cols) {
+    printf("M is NOT a magic square!\n");
+    exit(0);
+  }
+
+  matrix = malloc(sizeof(int) * rows * rows);
+  if (matrix == NULL) {
+    printf("Malloc error.  Exiting...\n");
+    exit(1);
+  }
+  // read in values
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < rows; j++) {
+      scanf(" %d", &matrix[i * rows + j]);
+    }
+  }
+
+  // re-print matrix to make sure everything was read correctly
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < rows; j++) {
+      printf("%d ", matrix[i * rows + j]);
+    }
+    printf("\n");
+  }
+
+  // get sum of first row
+  for (int i = 0; i < rows; i++) {
+    magic_const += matrix[i];
+  }
+  // printf("magic constant = %d\n", magic_const);
+
+  // check for matching sum in all other rows
+  for (int i = 1; i < rows; i++) {
+    int sum = 0;
+    for (int j = 0; j < rows; j++) {
+      sum += matrix[i * rows + j];
+    }
+    // printf("row sum = %d\n", sum);
+    if (sum != magic_const) {
+      printf("M is NOT a magic square!\n");
+      free(matrix);
+      matrix = NULL;
+      exit(0);
+    }
+  }
+
+  // check for matching sum in every column
+  for (int i = 0; i < rows; i++) {
+    int sum = 0;
+    for (int j = 0; j < rows; j++) {
+      sum += matrix[j * rows + i];
+    }
+    // printf("col sum = %d\n", sum);
+    if (sum != magic_const) {
+      printf("M is NOT a magic square!\n");
+      free(matrix);
+      matrix = NULL;
+      exit(0);
+    }
+  }
+
+  // check for matching sum in both diagonals
+  int sum = 0;
+  for (int i = 0; i < rows; i++) {
+    sum += matrix[i * rows + i];
+  }
+  // printf("diagonal sum = %d\n", sum);
+  if (sum != magic_const) {
+    printf("M is NOT a magic square!\n");
+    free(matrix);
+    matrix = NULL;
+    exit(0);
+  }
+
+  sum = 0;
+  for (int i = 0; i < rows; i++) {
+    sum += matrix[i * rows + (rows - 1 - i)];
+  }
+  // printf("diagonal sum = %d\n", sum);
+  if (sum != magic_const) {
+    printf("M is NOT a magic square!\n");
+    free(matrix);
+    matrix = NULL;
+    exit(0);
+  }
+
+  printf("M is a magic square (magic constant = %d)\n", magic_const);
+
+  free(matrix);
+  matrix = NULL;
+
+  return 0;
+}
--- A03/sorted_snackbar.c
+++ A03/sorted_snackbar.c
@@ -1,8 +1,9 @@
 //---------------------------------------------------------------------
-// sorted_snackbar.c 
+// sorted_snackbar.c
 // CS223 - Spring 2022
 // Ask the user for a list of snacks and store them in alphabetical order
-// Name:
+// Name: Jasmine Lei
+// Date: 11 February 2022
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,26 +16,95 @@ struct snack {
   struct snack* next;
 };
 
-// Insert a new node to a list (implemented as a linked list). 
+// Insert a new node to a list (implemented as a linked list).
 // The new node should store the given properties
 // Param snacks: the first item in the list (NULL if empty)
 // Param name: the snack name (max length is 32 characters)
 // Param quantity: the snack quantity
 // Param cost: the snack cost
 // Returns the first item in the list
-struct snack* insert_sorted(struct snack* snacks, 
-  const char* name, int quantity, float cost) {
+struct snack* insert_sorted(struct snack* snacks,
+    const char* name, int quantity, float cost) {
+  struct snack * new_snack = NULL;
+  new_snack = malloc(sizeof(struct snack));
+  if (new_snack == NULL) {
+    printf("Failed malloc, returning unchanged list.\n");
+    return snacks;
+  }
+  strcpy(new_snack->name, name);
+  new_snack->quantity = quantity;
+  new_snack->cost = cost;
+  new_snack->next = NULL;
 
-  // todo
-  return NULL;
+  if (snacks == NULL) {
+    // this new snack is the first to be added to the list
+    return new_snack;
+  }
+
+  if (strcmp(snacks->name, new_snack->name) > 0) {
+    // new_snack goes at the front of the list
+    new_snack->next = snacks;
+    return new_snack;
+  }
+  // otherwise find the correct position to maintain alpha order;
+  // also handles case of insertion at end of list
+  struct snack * temp = snacks;
+  while (temp->next != NULL && strcmp(temp->next->name, new_snack->name) <= 0) {
+    temp = temp->next;
+  }
+  // new snack should now be inserted after temp
+  new_snack->next = temp->next;
+  temp->next = new_snack;
+  return snacks;  // first item in list unchanged
 }
 
 // Delete (e.g. free) all nodes in the given list of snacks
 // Param snacks: the first node in the list (NULL if empty)
 void clear(struct snack* snacks) {
+  if (snacks == NULL) {
+    return;
+  }
+  struct snack * temp = snacks;
+  while (snacks != NULL) {
+    temp = snacks->next;
+    free(snacks);
+    snacks = temp;
+  }
+  // here, all nodes freed and snacks == NULL, as desired
 }
 
 int main() {
+  int snack_amount;
+  char name[32];
+  int quantity;
+  float cost;
+  struct snack * snacks = NULL;
+
+  printf("Enter a number of snacks: ");
+  scanf("%d", &snack_amount);
+
+  // fill with snacks
+  for (int i = 0; i < snack_amount; i++) {
+    printf("Enter a name: ");
+    scanf(" %s", name);
+    printf("Enter a cost: ");
+    scanf("%f", &cost);
+    printf("Enter a quantity: ");
+    scanf("%d", &quantity);
+    snacks = insert_sorted(snacks, name, quantity, cost);
+  }
+
+  // print current list of snacks
+  printf("\nWelcome to the Snack Bar.\n\n");
+  struct snack * temp = snacks;
+  int i = 0;
+  while (snacks != NULL) {
+    printf("%d) %-32s cost: $%-10.2f\t quantity: %d\n", i, temp->name,
+        temp->cost, temp->quantity);
+    temp = snacks->next;
+    i++;
+  }
+
+  clear(snacks);
   return 0;
 }
-
