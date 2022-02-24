1121d8acf6218628d830cbd27edb452f673bf060
eupatheia <75283980+eupatheia@users.noreply.github.com>
2022-02-24 15:04:59 -0500

    jam 2 code

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -15,17 +15,31 @@ struct cake makeCake(const char* flavor, float cost) {
   return newCake;
 }
 
-// todo: implement cheapestCake
+void cheapestCake(struct cake * head) {
+  struct cake * next = head;
+  struct cake * cheapest = NULL;
+  float min = next->cost;
+  while(next != NULL) {
+    printf("cake: %s ($%.02f)\n", next->flavor, next->cost);
+    if (next->cost < min) {
+      min = next->cost;
+      cheapest = next;
+    }
+    next = next->next;
+  }
+  printf("The cheapest cake is %s\n", cheapest->flavor);
+}
 
 int main() {
   struct cake cake1 = makeCake("red velvet", 2.00);
   struct cake cake2 = makeCake("chocolate", 1.75);
   struct cake cake3 = makeCake("mocha", 3.50);
-  // todo: define 4th cake
+  struct cake cake4 = makeCake("tea", 2.50);
 
   cake1.next = &cake2;
   cake2.next = &cake3;
+  cake3.next = &cake4;
   // draw stack and heap here
 
-  // todo: call cheapestCake
+  cheapestCake(&cake1);
 }
--- J02/readvector.c
+++ J02/readvector.c
@@ -5,8 +5,27 @@
 // The size of the vector should be stored in the parameter size
 // The values should be returned in a flat float array
 float* readvector(const char* filename, int *size) {
-  // todo: implement me
-  return NULL;
+  FILE* file;
+  char buffer[256];
+  int num;
+  float* vector;
+
+  file = fopen(filename, "r");
+  if (file == NULL) {
+    printf("File not found.  Exiting...");
+    exit(0);
+  }
+  fgets(buffer, 256, file);
+  sscanf(buffer, "%d", &num);
+  vector = malloc(sizeof(float) * num);
+
+  for (int i = 0; i < num; i++) {
+    fgets(buffer, 256, file);
+    sscanf(buffer, "%f", &vector[i]);
+  }
+  fclose(file);
+  *size = num;
+  return vector;
 }
 
 int main(int argc, char** argv) {
@@ -18,9 +37,11 @@ int main(int argc, char** argv) {
   int size = 0;
   float* vector = readvector(argv[1], &size);
 
-  // todo: print vector values
+  for (int i = 0; i < size; i++) {
+    printf("%f\n", vector[i]);
+  }
 
   free(vector);
+  vector = NULL;
   return 0;
 }
-
