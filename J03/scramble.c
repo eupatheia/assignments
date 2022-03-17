be3b67f0a9b5be4c46d8ab38e9cce8abd8a4f902
eupatheia <75283980+eupatheia@users.noreply.github.com>
2022-03-17 15:46:24 -0400

    jam 03 code

--- J03/scramble.c
+++ J03/scramble.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
 int main() {
+  char phrase[100];
+
+  printf("Please enter a phrase: ");
+  fgets(phrase, 100, stdin);
+
+  for (int i = 0; i < strlen(phrase); i++) {
+    unsigned char ch = phrase[i];
+    unsigned char second = 0x02;
+    unsigned char third = 0x04;
+
+    unsigned char secondch = ch & second;
+    unsigned char thirdch = ch & third;
+
+    if ((secondch >> 1) != (thirdch >> 2)) {
+      // sec = (ch & second) ^ 1;
+      // thir = (ch & third) ^ 1;
+      secondch = secondch << 1;
+      if (secondch != thirdch) {
+        ch = ch ^ 0x04;
+      }
+      thirdch = thirdch >> 1;
+      if (thirdch != secondch) {
+        ch = ch ^ 0x02;
+      }
+    }
+    phrase[i] = ch;
+  }
+  printf("scramble: %s\n", phrase);
+
   return 0;
 }
