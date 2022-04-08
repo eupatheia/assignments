de0c56f94793e21be807fdf1d85b453eca5d9a54
jlei@goldengate <jlei@goldengate.cs.brynmawr.edu>
2022-04-08 02:28:22 -0400

    functional single_mandelbrot

--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -12,7 +12,16 @@ int main(int argc, char* argv[]) {
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
+  struct ppm_pixel ** pixels = NULL;
+  struct ppm_pixel * palette = NULL;
+  unsigned int basered, basegreen, baseblue;
+  float xfrac, yfrac, x0, y0, x, y, xtmp;
+  int iter;
+  double timer;
+  struct timeval tstart, tend;
+  char new_file[100];
 
+  // process command line arguments
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
     switch (opt) {
@@ -28,10 +37,88 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
+  // attempt to allocate a 2D array of pixels
+  pixels = malloc(sizeof(struct ppm_pixel *) * size);
+  if (pixels == NULL) {
+    printf("Error: failed malloc.  Exiting...\n");
+    exit(1);
+  }
+  for (int i = 0; i < size; i++) {
+    pixels[i] = malloc(sizeof(struct ppm_pixel) * size);
+    if (pixels[i] == NULL) {
+      printf("Error: failed malloc.  Exiting...\n");
+      exit(1);
+    }
+  }
+
+  // generate palette
+  palette = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  if (palette == NULL) {
+    printf("Error: failed malloc.  Exiting...\n");
+    exit(1);
+  }
   srand(time(0));
+  basered = rand() % 255;
+  basegreen = rand() % 255;
+  baseblue = rand() % 255;
+  for (int i = 0; i < maxIterations; i++) {
+    palette[i].red = basered + rand() % 100 - 50;
+    palette[i].green = basegreen + rand() % 100 - 50;
+    palette[i].blue = baseblue + rand() % 100 - 50;
+  }
+
+  gettimeofday(&tstart, NULL);
 
   // compute image
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      xfrac = (float) i / size;
+      yfrac = (float) j / size;
+      x0 = xmin + xfrac * (xmax - xmin);
+      y0 = ymin + yfrac * (ymax - ymin);
 
+      x = 0;
+      y = 0;
+      iter = 0;
+      while (iter < maxIterations && x*x + y*y < 2*2) {
+        xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+	iter++;
+      }
+      if (iter < maxIterations) {
+        // escaped
+	pixels[i][j].red = palette[iter].red;
+	pixels[i][j].green = palette[iter].green;
+	pixels[i][j].blue = palette[iter].blue;
+      } else {
+	// did not escape, use color black
+        pixels[i][j].red = 0;
+        pixels[i][j].green = 0;
+        pixels[i][j].blue = 0;
+      }
+    }
+  }
+
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %g seconds\n", size, size, timer);
+
+  // write to file
+  new_file[0] = '\0';
+  sprintf(new_file, "mandelbrot-%d-%lu.ppm", size, time(0));
+  printf("Writing file %s\n", new_file);
+  write_ppm(new_file, pixels, size, size);
+
+  // free allocated array memory
+  for (int i = 0; i < size; i++) {
+    free(pixels[i]);
+    pixels[i] = NULL;
+  }
+  free(pixels);
+  pixels = NULL;
+  free(palette);
+  palette = NULL;
+
+  return 0;
 }
